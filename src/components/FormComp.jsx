814ba19b153c13f8b3c6de4a70dc1220baaeacef
K-Wuthichai-7 <getthai.13.2540@gmail.com>
2024-02-01 13:43:18 +0700

    update 1/2/2567

---
+++ src/components/FormComp.jsx
@@ -0,0 +1,209 @@
+import "../css/Form.css";
+export const FormComp = () => {
+  return (
+    <>
+      {/* <h2 className="text-center">PTT GLOBAL</h2> */}
+
+      <form className="form-horizontal">
+        <div className="container text-center sticky-top">
+          <div className="row align-items-start header-table py-3">
+            <div className="col-md-8 text-light">Property</div>
+            <div className="col-md-4 text-light">Result</div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row px-3 py-2">
+          <div className="col-md-8 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 ">
+            <div className="form-group">
+              <div className="col-sm-12">
+                <input type="text" className="form-control" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/components/Ocr.jsx
+++ src/components/Ocr.jsx
@@ -1,7 +1,6 @@
 import Pdfview from './Pdfview'
+import { FormComp } from './FormComp'
 import '../css/ocr.css'
-import Table from 'react-bootstrap/Table'
-
 
 export const Ocr = () => {
   return (
@@ -11,85 +10,9 @@ export const Ocr = () => {
         <Pdfview/>
         </div>
         </div>
-
         <div className="col-lg-6 d-flex justify-content-center" >
            <div className="card boxbar">
-              <Table striped bordered hover>             
-                <thead>
-                  <tr>
-                    <th style={{ textAlign: 'center' }}>PROPERTY</th>
-                    <th style={{ textAlign: 'center' }}>RESULT </th>
-                  </tr>
-                </thead>
-          
-                <tbody>
-                  <tr>
-                    <td>1</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>2</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text' /></td>
-                  </tr>
-                  <tr>
-                    <td>3</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>4</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>5</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td style={{ textAlign: 'center' }} ><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-                  <tr>
-                    <td>6</td>
-                    <td  style={{ textAlign: 'center' }}><input type='text'/></td>
-                  </tr>
-
-                </tbody>
-              </Table>
+         <FormComp/>     
           </div>
         </div>
     </div>
