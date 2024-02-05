d9d3c71d7cfe1c110378fd4c255c3ce43d1b963a
K-Wuthichai-7 <getthai.13.2540@gmail.com>
2024-02-05 10:20:08 +0700

    'add-Zoom'

--- src/components/IconComponent.jsx
+++ src/components/IconComponent.jsx
@@ -11,7 +11,7 @@ function IconComponent() {
             <Combobox/>
           </div>
           <div className="col-md-4 text-center ">
-            <form className="input-group">
+            <form className="input-group" action="" method="POST">
               <input type="file" name="file" className="form-control" />
               <Button className="px-2" type="submit">
                 Save <i className="bi bi-floppy2-fill" />
@@ -19,7 +19,6 @@ function IconComponent() {
             </form>
           </div>
           <div className="col-md-4" >
-           
           </div>
         </div>
       </div>
--- src/components/Pdfview.jsx
+++ src/components/Pdfview.jsx
@@ -6,90 +6,125 @@ import "cropperjs/dist/cropper.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const Pdfview = () => {
-    const cropperRef = useRef(null);
-    // const [croppedImageData, setCroppedImageData] = useState(null);
+  const cropperRef = useRef(null);
 
-    // แจ้งเตือนการ Fetch Api
-    
-    const resolveAfter = () => {
-        const fetchApi = fetch('https://jsonplaceholder.typicode.com/posts')
 
-        toast.promise(
-            new Promise(async (resolve, reject) => {
-                try {
-                    const response = await fetchApi;
-                    const data = await response.json();
-    
-                    // ทำการใช้ข้อมูลที่ได้จากการ fetch API ได้ตรงนี้
-                    console.log("ข้อมูลจาก fetch API:", data);
-    
-                    setTimeout(() => {
-                        resolve();
-                    }, 2000);
-                } catch (error) {
-                    reject(error);
-                }
-            }),
-            {
-                pending: "กำลังประมวลผล",
-                success: "สำเร็จ",
-                error: "เกิดข้อผิดพลาด",
-            },
-            {}
-        );
-    };
-    // จบแจ้งเตือน
+  // แจ้งเตือนการ Fetch Api
+  const resolveAfter = () => {
+    const fetchApi = fetch("https://jsonplaceholder.typicode.com/posts");
 
-    // Crop
-    const onCrop = () => {
-        const cropper = cropperRef.current?.cropper;
-        const imageData = cropper.getCroppedCanvas().toDataURL();
-        // setCroppedImageData(imageData);
+    toast.promise(
+      new Promise(async (resolve, reject) => {
+        try {
+          const response = await fetchApi;
+          const data = await response.json();
 
-        // ทำการส่งข้อมูลภาพที่ถูก crop ไปยัง URL /upload
-        uploadCroppedImage(imageData);
-    };
+          // ทำการใช้ข้อมูลที่ได้จากการ fetch API ได้ตรงนี้
+          console.log("ข้อมูลจาก fetch API:", data);
 
-    const uploadCroppedImage = (imageData) => {
-        fetch("http://127.0.0.1:5000/upload", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ image: imageData }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("รับข้อมูลจาก /upload:", data);
-            })
-            .catch((error) => {
-                console.error("เกิดข้อผิดพลาดในการส่งข้อมูล:", error);
-            });
-    };
-
-    return (
-        <>
-            <Cropper
-                src="../img/BSRC_GB2.jpg"
-                style={{ height: "600px", width: "100%" }}
-                initialAspectRatio={16 / 9}
-                guides={false}
-                ref={cropperRef}
-            />
-            <ToastContainer position="top-left" />
-            <Button
-                variant="outline-primary"
-                className="mt-3 w-100"
-                onClick={() => {
-                    onCrop();
-                    resolveAfter();
-                }}
-            >
-                ประมวลผล
-            </Button>
-            {/* {croppedImageData && <img src={croppedImageData} alt="Cropped Image" />} */}
-        </>
+          setTimeout(() => {
+            resolve();
+          }, 2000);
+        } catch (error) {
+          reject(error);
+        }
+      }),
+      {
+        pending: "กำลังประมวลผล",
+        success: "สำเร็จ",
+        error: "เกิดข้อผิดพลาด",
+      },
+      {}
     );
+  };
+  // จบการแจ้งเตือน
+
+
+  //   Zoom
+
+  const zoomIn = () => {
+    cropperRef.current?.cropper.zoom(0.1);
+  };
+  const zoomOut = () => {
+    cropperRef.current?.cropper.zoom(-0.1);
+  };
+
+  //End Zoom
+
+
+  // Crop
+
+  const onCrop = () => {
+    const cropper = cropperRef.current?.cropper;
+    const imageData = cropper.getCroppedCanvas().toDataURL();
+  
+
+    // ทำการส่งข้อมูลภาพที่ถูก crop ไปยัง URL /upload
+    uploadCroppedImage(imageData);
+  };
+
+  const uploadCroppedImage = (imageData) => {
+    fetch("http://127.0.0.1:5000/upload", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ image: imageData }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("รับข้อมูลจาก /upload:", data);
+      })
+      .catch((error) => {
+        console.error("เกิดข้อผิดพลาดในการส่งข้อมูล:", error);
+      });
+  };
+
+  return (
+    <>
+      <Cropper
+        src="../img/BSRC_GB2.jpg"
+        style={{ height: "600px", width: "100%" }}
+        initialAspectRatio={16/9}
+        guides={false}
+        ref={cropperRef}
+      />
+      <ToastContainer position="top-left" />
+      <div className="d-flex">
+        <div className="d-flex">
+        <Button
+          variant="btn btn-primary"
+          className="mt-3 me-2 w-100"
+          onClick={() => {
+            zoomIn();
+          }}
+        >
+          <i className="bi bi-zoom-in"></i>
+        </Button>
+        <Button
+          variant="btn btn-primary"
+          className="mt-3 me-2 w-100"
+          onClick={() => {
+            zoomOut();
+          }}
+        >
+          <i className="bi bi-zoom-out"></i>
+        </Button>
+        </div>
+        <Button
+          variant="btn btn-primary"
+          className="mt-3  w-100"
+          onClick={() => {
+            onCrop();
+            resolveAfter();
+          }}
+        >
+          ประมวลผล
+        </Button>
+      </div>
+     
+    </>
+  );
 };
 
 export default Pdfview;
