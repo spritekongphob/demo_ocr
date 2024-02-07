57217d206a027115d29f95acc575cf2ae4655f44
K-Wuthichai-7 <getthai.13.2540@gmail.com>
2024-02-07 11:14:33 +0700

    selectimages

--- src/components/Pdfview.jsx
+++ src/components/Pdfview.jsx
@@ -6,24 +6,87 @@ import axios from "axios";
 import "cropperjs/dist/cropper.css";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const Pdfview = () => {
   const cropperRef = useRef(null);
   const [file, setFile] = useState(null);
-  const [imageData, setImageData] = useState("");
+  const [imagesData, setImagesData] = useState([]);
+  const [selectedFileIndex, setSelectedFileIndex] = useState(0);
 
   useEffect(() => {
     // โหลดข้อมูลภาพจาก Backend
     axios
-      .get("http://localhost:5000/get_image")
+      .get("http://localhost:5000/get_images")
       .then((response) => {
-        console.log("Response from backend:", response.data); // ล็อกค่าที่ได้รับ
-        setImageData(response.data.imageData);
+        console.log("Response from backend:", response.data);
+        setImagesData(response.data.imagesData);
       })
       .catch((error) => {
-        console.error("Error fetching image:", error);
+        console.error("Error fetching images:", error);
       });
   }, []);
+
+  const zoomIn = () => {
+    cropperRef.current?.cropper.zoom(0.1);
+  };
+
+  const zoomOut = () => {
+    cropperRef.current?.cropper.zoom(-0.1);
+  };
+
+  // Crop
+  const onCrop = () => {
+    const croppedCanvas = cropperRef.current.cropper.getCroppedCanvas();
+
+    // Convert the cropped canvas to a data URL with the desired format (in this case, image/png)
+    const croppedImageData = croppedCanvas.toDataURL("image/png");
+
+    // Convert the data URL to a Blob
+    const blob = dataURItoBlob(croppedImageData);
+
+    // Convert the Blob to a File with the desired file name and type
+    const croppedFile = new File([blob], "crop-image.png", {
+      type: "image/png",
+    });
+
+    setFile(croppedFile);
+  };
+
+  const handleFileUpload = (e) => {
+    e.preventDefault();
+
+    if (file) {
+      // ส่งภาพไปที่ Backend
+      const formData = new FormData();
+      formData.append("file", file);
+
+      fetch("http://localhost:5500/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.text())
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("Error uploading image", error);
+        });
+    }
+  };
+
+  // แปลง Url เป็น ภาพ
+  const dataURItoBlob = (dataURI) => {
+    const byteString = atob(dataURI.split(",")[1]);
+    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
+
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ab], { type: mimeString });
+  };
 
   // แจ้งเตือนการ Fetch Api
   const resolveAfter = () => {
@@ -55,89 +118,50 @@ const Pdfview = () => {
   };
   // จบการแจ้งเตือน
 
-  const zoomIn = () => {
-    cropperRef.current?.cropper.zoom(0.1);
-  };
-  const zoomOut = () => {
-    cropperRef.current?.cropper.zoom(-0.1);
-  };
-
-  // Crop
-
-  const onCrop = () => {
-
-    const croppedCanvas = cropperRef.current.cropper.getCroppedCanvas();
-
-    // Convert the cropped canvas to a data URL with the desired format (in this case, image/png)
-    const croppedImageData = croppedCanvas.toDataURL("image/png");
-
-    // Convert the data URL to a Blob
-    const blob = dataURItoBlob(croppedImageData);
-
-    // Convert the Blob to a File with the desired file name and type
-    const croppedFile = new File([blob], "crop-image.png", {
-      type: "image/png",
-    });
-
-    setFile(croppedFile);
-  };
-
-  const handleFileChange = (e) => {
-    // Get the selected file from the input field
-    const selectedFile = e.target.files[0];
-    setFile(selectedFile);
-  };
-
-  const handleFileUpload = (e) => {
-    e.preventDefault();
-
-    if (file) {
-      // ส่งภาพไปที่ Backend
-      const formData = new FormData();
-      formData.append("file", file);
-
-      fetch("http://localhost:5000/upload", {
-        method: "POST",
-        body: formData,
-      })
-        .then((response) => response.text())
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error("Error uploading image", error);
-        });
-    }
-  };
-
-  // แปลง Url เป็น ภาพ
-  const dataURItoBlob = (dataURI) => {
-    const byteString = atob(dataURI.split(",")[1]);
-    const mimeString = dataURI.split(",")[0].split(":")[1].split(";")[0];
-
-    const ab = new ArrayBuffer(byteString.length);
-    const ia = new Uint8Array(ab);
-
-    for (let i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ab], { type: mimeString });
-  };
-
   return (
     <>
       <Cropper
-        // src={`data:image/png;base64,${imageData}`}
-        src="../img/BSRC_GB1.jpg"
+        src={
+          imagesData.length > 0 && imagesData[selectedFileIndex]
+            ? `data:image/png;base64,${imagesData[selectedFileIndex].imageData}`
+            : ""
+        }
         style={{ height: "600px", width: "100%" }}
-        initialAspectRatio={16 / 9}
+        initialAspectRatio={1/1}
         guides={false}
         ref={cropperRef}
       />
+
+      {/* แสดงแจ้งเตือน บน-ซ้าย */}
       <ToastContainer position="top-left" />
+
       <div className="d-flex justify-content-center">
         <div className="d-flex">
+          <Button
+            className="mt-3 me-2 w-100"
+            variant="btn btn-primary"
+            onClick={() => {
+              if (selectedFileIndex > 0) {
+                setSelectedFileIndex(selectedFileIndex - 1);
+              }
+            }}
+          >
+            {/* previous icon*/}
+            <i class="bi bi-arrow-left-short"></i>
+          </Button>
+          <Button
+            className="mt-3 me-2 w-100"
+            variant="btn btn-primary"
+            onClick={() => {
+              if (selectedFileIndex < imagesData.length - 1) {
+                setSelectedFileIndex(selectedFileIndex + 1);
+              }
+            }}
+          >
+            {/* next icon*/}
+            <i class="bi bi-arrow-right-short"></i>
+          </Button>
+
           <Button
             variant="btn btn-primary"
             className="mt-3 me-2 w-100"
@@ -158,23 +182,29 @@ const Pdfview = () => {
           </Button>
         </div>
 
-        <form onSubmit={handleFileUpload}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleFileUpload(e);
+          }}
+        >
           <input
             type="hidden"
             name="file"
             accept="image/*"
-            onChange={handleFileChange}
+            onChange={(e) => {
+              const selectedFile = e.target.files[0];
+              setFile(selectedFile);
+            }}
           />
-          {/* <input type="submit" value="Upload" onClick={onCrop}/> */}
           <Button
             variant="btn btn-primary"
-            className="mt-3  w-100"
+            className="mt-3 w-100"
             onClick={() => {
               onCrop();
               resolveAfter();
             }}
             type="submit"
-            value="Upload"
           >
             ประมวลผล
           </Button>
